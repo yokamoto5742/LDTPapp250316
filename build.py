d98822b32de8760e71487725cac782c9a50b11f7
yokamoto5742-h <yokamoto5742@gmail.com>
2025-03-16 19:10:28 +0900

    1st commit

--- build.py
+++ build.py
@@ -1,30 +1,22 @@
 import subprocess
-import shutil
 import os
-from version_manager import update_version, update_main_py
+from version_manager import update_version, update_version_py
 
 
 def build_executable():
-    # バージョンを更新
     new_version = update_version()
-    update_main_py(new_version)
+    update_version_py(new_version)
 
     subprocess.run([
         "pyinstaller",
         "--name=LDTPapp",
         "--windowed",
         "--icon=assets/LDPTapp_icon.ico",
+        "--add-data", "config.ini;.",
         "main.py"
     ])
 
-    # 必要なファイルをdistフォルダにコピー
-    shutil.copy("config.ini", "dist/LDTPapp")
-    excel_src = r"C:\Shinseikai\LDTPapp\LDTPform.xlsm"
-    excel_dst = os.path.join("dist/LDTPapp", "LDTPform.xlsm")
-    shutil.copy(excel_src, excel_dst)
-
     print(f"Executable built successfully. Version: {new_version}")
-    print(f"Excel file copied to: {excel_dst}")
 
 
 if __name__ == "__main__":
